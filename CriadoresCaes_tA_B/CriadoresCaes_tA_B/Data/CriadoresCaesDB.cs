a5b3d2374d66f6ceaf25badfcd93de07cde1a700
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-03-24 12:37:02 +0000

    Expecificação da BD

---
+++ CriadoresCaes_tA_B/CriadoresCaes_tA_B/Data/CriadoresCaesDB.cs
@@ -0,0 +1,31 @@
+﻿using CriadoresCaes_tA_B.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CriadoresCaes_tA_B.Data {
+
+   /// <summary>
+   /// esta classe representa a Base de Dados a ser utilizada neste projeto
+   /// </summary>
+   public class CriadoresCaesDB : DbContext {
+
+      // construtor da classe CriadoresCaesDB
+      // indicar onde está a BD à qual estas classes (tabelas) serão associadas
+      // ver o conteúdo do ficheiro 'startup.cs'
+      public CriadoresCaesDB(DbContextOptions<CriadoresCaesDB> options):base(options) { }
+
+      //Representar as Tabelas da BD
+      public DbSet<Criadores> Criadores { get; set; }
+      public DbSet<Caes> Caes { get; set; }
+      public DbSet<Racas> Racas { get; set; }
+      public DbSet<Fotografias> Fotografias { get; set; }
+      public DbSet<CriadoresCaes> CriadoresCaes { get; set; }
+
+
+   }
+}
--- CriadoresCaes_tA_B/CriadoresCaes_tA_B/Startup.cs
+++ CriadoresCaes_tA_B/CriadoresCaes_tA_B/Startup.cs
@@ -1,9 +1,14 @@
+using CriadoresCaes_tA_B.Data;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+// adicionar esta referência para q a EF seja invocada
+using Microsoft.EntityFrameworkCore;
 
 using System;
 using System.Collections.Generic;
@@ -18,10 +23,27 @@ namespace CriadoresCaes_tA_B {
 
       public IConfiguration Configuration { get; }
 
+
+
+
       // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services) {
          services.AddControllersWithViews();
+
+         //************************************************************
+         // configurar o acesso à BD
+         //************************************************************
+         services.AddDbContext<CriadoresCaesDB>(
+            options => options.UseSqlServer( Configuration.GetConnectionString("myConnectionString")  )
+            );
+
+         //************************************************************
+
+
       }
+
+
+
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
       public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
